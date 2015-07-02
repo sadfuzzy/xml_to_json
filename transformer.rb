22052718de2e62654f60047f7145a82a27876e4e
Denis Savitskiy <sadfuzzy@yandex.ru>
2015-07-02 14:33:18 +0300

    Convert to json with active_support/core_ext.

--- transformer.rb
+++ transformer.rb
@@ -1,10 +1,7 @@
 #!/usr/bin/env ruby
+require 'active_support'
+require 'active_support/core_ext'
 
-require 'nokogiri'
-
-document = Nokogiri::XML(File.read('Test List Applet.xml'))
-template = Nokogiri::XSLT(File.read('repo_to_json.xslt'))
-
-transformed_document = template.transform(document)
-
+xml = File.read('Test List Applet.xml')
+transformed_document = Hash.from_xml(xml).to_json
 File.open('output.json', 'w').write(transformed_document)
